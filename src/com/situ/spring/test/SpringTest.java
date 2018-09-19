ff12151dffa634c8a106ec8440137e198d261072
Lucky-Rui <1269552287@qq.com>
2018-09-19 22:22:53 +0800

    IOC、DI

--- src/com/situ/spring/test/SpringTest.java
+++ src/com/situ/spring/test/SpringTest.java
@@ -1,5 +1,14 @@
 package com.situ.spring.test;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SpringTest {
-	
+	@Test
+	public void test01() {
+		//得到Spring容器
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//从Spring中取出bean
+	}
 }
