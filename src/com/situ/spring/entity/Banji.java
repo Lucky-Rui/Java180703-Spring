75b304c67b0b0cccc41e2a8e43094df9234efb97
Lucky-Rui <1269552287@qq.com>
2018-09-19 17:13:06 +0800

    初始化Spring项目

---
+++ src/com/situ/spring/entity/Banji.java
@@ -0,0 +1,52 @@
+package com.situ.spring.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banji {
+	private Integer id;
+	private String name;
+	// 一个班可以学习多门课程，一对多
+	private List<Course> courseList = new ArrayList<>();
+	// 一个班有多个学生，一对多
+	private List<Student> studentList = new ArrayList<>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Course> getCourseList() {
+		return courseList;
+	}
+
+	public void setCourseList(List<Course> courseList) {
+		this.courseList = courseList;
+	}
+
+	public List<Student> getStudentList() {
+		return studentList;
+	}
+
+	public void setStudentList(List<Student> studentList) {
+		this.studentList = studentList;
+	}
+
+	@Override
+	public String toString() {
+		return "Banji [id=" + id + ", name=" + name + ", courseList=" + courseList + ", studentList=" + studentList
+				+ "]";
+	}
+
+}
---
+++ src/com/situ/spring/entity/Student.java
@@ -0,0 +1,58 @@
+package com.situ.spring.entity;
+
+public class Student {
+	private Integer id;
+	private String name;
+	private Integer age;
+	private String gender;
+	// 一个学生属于一个班级，一对一关系
+	// private Integer banjiId;
+	private Banji banji;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Banji getBanji() {
+		return banji;
+	}
+
+	public void setBanji(Banji banji) {
+		this.banji = banji;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", banji=" + banji
+				+ "]";
+	}
+
+}
